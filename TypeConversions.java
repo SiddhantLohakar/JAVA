43f60d108f0bccfe006dbd32f70794daf6264c47
Siddhant Lohakar <siddhantlohakar12@gmail.com>
2026-04-17 22:53:09 +0530

    Type conversion

--- TypeConversions.java
+++ TypeConversions.java
@@ -7,5 +7,15 @@ public class TypeConversions {
         System.out.println("Widening1 byte to int: " + b + " " + i);
         i = c;
         System.out.println("Widening2: char to int: " + c + " " + i);
+
+        // Narrowing Conversion
+        i = 300;
+        b = (byte) i;
+         System.out.println("Narrowing int to byte: " + i + " " + b);
+
+        //  Truncating Conversion
+        float f = 123.45f;
+        i = (int) f;
+        System.out.println("Truncating Conversion: " + f + " " + i);
     }
 }
