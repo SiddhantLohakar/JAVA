939df8548658403ae302d8caa8bc9df3f40c1d82
Siddhant Lohakar <siddhantlohakar12@gmail.com>
2026-04-18 15:16:37 +0530

    flow control code

---
+++ flowControl.java
@@ -0,0 +1,33 @@
+public class flowControl {
+    public static void main(String[] args) {
+
+        boolean b = true;
+        // int a = 30;
+
+        if(b)
+        {
+            System.out.println("b is true");
+        }
+        else
+        {
+            System.out.println("b is false");
+        }
+
+
+        // Switch Statements
+        int i = 2;
+
+        switch(i)
+        {
+            case 1:
+                System.out.println("i is 1");
+                break;
+            case 2:
+                System.out.println("i is 2");
+                break;
+            default:
+                System.out.println("Nothing");
+        }
+
+    }
+}
