f250f02c79d45da424c1506b7db2ac96e62accf7
Siddhant Lohakar <siddhantlohakar12@gmail.com>
2026-04-19 19:03:13 +0530

    Constructor chaining

---
+++ OOPS.java
@@ -0,0 +1,41 @@
+public class OOPS {
+    public static void main(String[] args) {
+        Student s1 = new Student();
+
+        // System.out.println(s1.name + " " +  s1.age + " " + s1.rollNumber);
+
+        //  Code for constructor chaining
+    }
+}
+
+class Student{
+    String name;
+    int age;
+    int rollNumber;
+
+    // Basic Constructor
+    // Student(){
+    //     name = "I";
+    //     age = 4;
+    //     rollNumber = 5;
+    // }
+
+    // Constructor Chaining
+    Student(){
+        this("unknown");
+    }
+
+    Student(String name){
+        this(name, 0);
+    }
+
+    Student(String name, int age){
+       this(name, age, 0);
+    }
+
+    Student(String name, int age, int rollNumber){
+        this.name = name;
+        this.age = age;
+        this.rollNumber = rollNumber;
+    }
+}
