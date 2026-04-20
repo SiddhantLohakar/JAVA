7b1ed521b86814320218cc30d8c6cd062b1ce923
Siddhant Lohakar <siddhantlohakar12@gmail.com>
2026-04-20 12:50:36 +0530

    Packages code

---
+++ Importing.java
@@ -0,0 +1,8 @@
+import School.Student;
+
+public class Importing {
+    public static void main(String[] args) {
+        Student s1 = new Student();
+        s1.print();
+    }
+}
---
+++ School/Student.java
@@ -0,0 +1,7 @@
+package School;
+
+public class Student{
+    public void print(){
+        System.out.println("Hello from School Package");
+    }
+}
