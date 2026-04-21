5d83785dc2919b7710e6eed47d6cfafeadc90fd9
Siddhant Lohakar <siddhantlohakar12@gmail.com>
2026-04-21 18:00:14 +0530

    Autoboxing and Unoxing code

---
+++ Boxing.java
@@ -0,0 +1,19 @@
+public class Boxing {
+    public static void main(String[] args) {
+        // int x = 10;
+        // Integer y = x;
+
+        // // Unboxing
+        // Integer a = new Integer(20);
+        // int b = a;
+
+        // System.out.println(y);
+        // System.out.println(b);
+
+
+        //  Comparing two values of Wrapper class:
+        
+
+
+    }
+}
