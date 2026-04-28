7fb9c4ceb74235b1668f468d9ea7fddda88ae521
Siddhant Lohakar <siddhantlohakar12@gmail.com>
2026-04-28 20:57:55 +0530

    Iterator code

---
+++ Iterations.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Iterations{
+    public static void main(String[] args) {
+        // List<Integer> list = new ArrayList<>();
+
+        // list.add(30);
+        // list.add(40);
+        // list.add(80);
+        // list.add(90);
+
+        // Iterator<Integer> it = list.iterator();
+
+        // while(it.hasNext()){
+        //     System.out.println(it.next());
+        // }
+    }
+}
